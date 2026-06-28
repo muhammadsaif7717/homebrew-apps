1ae032c72bc7ca17e9f856c7888406f49160be15
MD. Saif Islam <muhammadsaif7717@gmail.com>
2026-06-29 00:44:29 +0600

    added macsync-and-restore.rb

---
+++ Casks/macsync-and-restore.rb
@@ -0,0 +1,19 @@
+cask "macsync-and-restore" do
+  version "1.0.0"
+  sha256 "74b71091ab2d2dd5c072d9720e43ce39f04b46904a8d1b86095aa3ed0a2e3bb2"
+
+  # We use %20 for spaces and %26 for the '&' symbol to ensure the URL resolves properly
+  url "https://github.com/muhammadsaif7717/macsync-and-restore/releases/download/v#{version}/MacSync%20%26%20Restore-#{version}-arm64.dmg"
+  name "MacSync & Restore"
+  desc "Backup, sync, and restore utility for macOS"
+  homepage "https://github.com/muhammadsaif7717/macsync-and-restore"
+
+  app "MacSync & Restore.app"
+
+  zap trash: [
+    "~/Library/Application Support/MacSync & Restore",
+    "~/Library/Preferences/com.macsyncandrestore.app.plist",
+    "~/Library/Logs/MacSync & Restore",
+    "~/Library/Saved Application State/com.macsyncandrestore.app.savedState",
+  ]
+end
