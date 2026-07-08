4929e043aa8c4dacd01cb3f2315c9dc13f6174a5
MD. SAIF ISLAM <muhammadsaif7717@gmail.com>
2026-07-08 22:31:28 +0600

    Update gdrive-connector.rb

--- Casks/gdrive-connector.rb
+++ Casks/gdrive-connector.rb
@@ -2,11 +2,11 @@ cask "gdrive-connector" do
   version "1.0.0"
   sha256 "bd5f2dc840ed85a39f2ab74092c1f3584df8aa73b40e78423bdb3ed0f99fcd2e"
 
-  # Matches your exact GitHub asset delivery name structure
   url "https://github.com/muhammadsaif7717/GDriveConnector/releases/download/v#{version}/G.Drive.Connector-#{version}-arm64.dmg"
+
   name "G Drive Connector"
   desc "A modern and seamless Google Drive desktop client"
-  homepage "https://github.com/vevonex/GDriveConnector"
+  homepage "https://github.com/muhammadsaif7717/GDriveConnector"
 
   app "G Drive Connector.app"
 
